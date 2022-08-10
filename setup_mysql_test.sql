fa5760fdee4e93a183e38ba6435589875ec1143b
MoonBeJPG <agustinasoledad2608@gmail.com>
2022-08-10 16:01:36 -0700

    Checking task four

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- sql
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_shema.* TO 'hbnb_test'@'localhost';
