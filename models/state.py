72fa7e2f897eba20e24a1ba8c159f7c8cf5693be
MoonBeJPG <agustinasoledad2608@gmail.com>
2022-08-10 18:15:14 -0700

    Checking some tasks of task siz

--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    @property
+    def cities(self):
+        """ getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id """
+        cities = []
+        for i, j in self.cities.items():
+            if self.id == j.state_id:
+                cities.append(j)
+        return cities
