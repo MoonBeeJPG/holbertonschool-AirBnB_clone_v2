3a202aaff7e19dce7928bb645e5d27df5a22c232
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-08-26 02:21:39 +0000

    commit

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Fabric script that distributes an archive to your web servers,
+    using the function do_deploy """
+
+from fabric.api import run, put, env
+from os.path import exists
+
+env.user = "ubuntu"
+env.hosts = ['3.88.99.250', '184.73.123.157']
+
+
+def do_deploy(archive_path):
+    """ distributes an archive to your web servers """
+    if exists(archive_path) is False:
+        return False
+    try:
+        put(archive_path, "/tmp/")
+        filename = archive_path.split("/")[1]
+        filename_no_extension = filename.split(".")[0]
+        file_path = "/data/web_static/releases/" + filename_no_extension + "/"
+        run("mkdir -p" + file_path)
+        run("tar -xf2 /tmp/" + filename + " -C" + file_path)
+        run("mv " + file_path + "web_static/*" + " " + file_path)
+        run("rm /tmp/{}".format(filename))
+        run("rm -rf " + file_path + "web_static")
+        run("rm -rf /data/web_static/current")
+        run("ln -s " + file_path + " /data/web_static/current")
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
