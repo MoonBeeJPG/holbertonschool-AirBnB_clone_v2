789c98751e723b28ae20a12c6ba0385c948d01a1
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-08-26 01:23:42 +0000

    commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployement of web_static
-sudo apt update
+sudo aptupdate
 sudo apt -y install nginx
-mkdir /data/web_static/releases/test
-mkdir /data/web_static/shared
-echo "
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo
+"<html>
+    <head>
+    </head>
+    <body>
+      Holberton School
+    </body>
+  </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sed -i "/listen 80 default_server;/a location /hbnb/static/ {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
