e457017fe1a4888c2a7460e4e6b5067c513d079e
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-08-26 01:25:46 +0000

    checking

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,14 +4,13 @@ sudo apt update
 sudo apt -y install nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-echo
-"<html>
-    <head>
-    </head>
-    <body>
-      Holberton School
-    </body>
-  </html>" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sed -i "/listen 80 default_server;/a location /hbnb/static/ {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
