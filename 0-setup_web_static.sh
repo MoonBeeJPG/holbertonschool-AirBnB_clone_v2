1dac63c495d8b1a505d55bece9ebea348d2fd3af
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-08-26 01:27:57 +0000

    Trying to fix final check again

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,7 @@
 # Bash script that sets up your web servers for the deployement of web_static
 sudo apt update
 sudo apt -y install nginx
-mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "<html>
   <head>
