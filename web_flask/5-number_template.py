ba4f3e72a4ef8b918a170dbd0ad39f6604ef871e
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-09-06 00:39:02 +0000

    commit

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,38 +4,39 @@
 
 if __name__ == "__main__":
     from flask import Flask
+    from flask import render_template
 
     app = Flask(__name__)
 
     @app.route('/', strict_slashes=False)
-    def route():
-        """ display Hello HBNB! """
+    def hello_hbnb():
+        """root folder route"""
         return "Hello HBNB!"
 
     @app.route('/hbnb', strict_slashes=False)
-    def route1():
-        """ display HBNB """
+    def hbnb_route():
+        """/hbnb/ folder route"""
         return "HBNB"
 
     @app.route('/c/<text>', strict_slashes=False)
-    def route2(text):
-        """ display C, followed by the value of the text variable """
+    def c_route(text):
+        """some text in output depending on url after /c/"""
         return "C " + text.replace('_', ' ')
 
     @app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
-    @app.route('/python/<text>', strict_slashes=False)
-    def route3(text):
-        """ display Python, followed by the value of the text variable """
+    @app.route('/python/(<text>', strict_slashes=False)
+    def python_route(text):
+        """text depending on url after"""
         return "Python " + text.replace('_', ' ')
 
     @app.route('/number/<int:n>', strict_slashes=False)
-    def route4(n):
-        """ display n is a numbe only if n is an integer """
+    def n_route(n):
+        """depending of the route"""
         return str(n) + " is a number"
 
     @app.route('/number_template/<int:n>', strict_slashes=False)
     def number_templates(n):
         """depending after of the url"""
-        return render_template('5-number.html', n=n) 
+        return render_template('5-number.html', n=n)
 
     app.run(host='0.0.0.0')
