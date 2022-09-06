768cda812261868c3fde6a12a78980bb63ac35f7
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-09-06 00:37:09 +0000

    commit

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,9 +33,4 @@ if __name__ == "__main__":
         """ display n is a numbe only if n is an integer """
         return str(n) + " is a number"
 
-    @app.route('/number_template/<int:n>', strict_slashes=False)
-    def route5(n):
-        """ display a HTML page only if n is an integer """
-         return render_template('5-number.html', n=n)
-
     app.run(host='0.0.0.0')
