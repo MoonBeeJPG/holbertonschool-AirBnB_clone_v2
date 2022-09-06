bd6bcd899e4134597f22aa066363c1714a8cc155
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-09-06 00:05:32 +0000

    More task to check

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+
+if __name__ == "__main__":
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def route():
+        """ Root folder route """
+        return "Hello HBNB!"
+
+    @app.route('/hbnb', strict_slashes=False)
+    def route1():
+        """ /hbnb folder route """
+        return "HBNB"
+
+    @app.route('/c/<text>', strict_slashes=False)
+    def route2(text):
+        """ /c/<text> folder route """
+        return "C " + text.replace('_', ' ')
+
+    @app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
+    @app.route('/python/<text>', strict_slashes=False)
+    def route3(text):
+        """ /python/(<text>) folder route """
+        return "Python " + text.replace('_', ' ')
+
+    @app.route('/number/<n>', strict_slashes=False)
+    def route4(n):
+        """ /number/n folder route """
+        if isinstance(n, int):
+            return "n is a number"
+
+    app.run(host='0.0.0.0')
